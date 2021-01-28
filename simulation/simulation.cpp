2ed69720ec7352e3644e97a903dfa60eaf76abc9
ginoscott <ginoscottdev@gmail.com>
2021-01-27 16:04:31 -1000

    simulation trails for past 100 points showing

--- simulation/simulation.cpp
+++ simulation/simulation.cpp
@@ -7,17 +7,19 @@ simulation::simulation() {
 void simulation::init_simulation() {
 	param = SPH.get_param();
 	group_conf = SPH.get_group_conf();
-	x = SPH.get_x();
-	y = SPH.get_y();
-	u = SPH.get_u();
-	v = SPH.get_v();
+	// n x 100 matrix
+	trackMax = 100;
+	x = SPH.get_x() * MatrixXd::Ones(1,trackMax);
+	y = SPH.get_y() * MatrixXd::Ones(1,trackMax);
+	u = SPH.get_u() * MatrixXd::Ones(1,trackMax);
+	v = SPH.get_v() * MatrixXd::Ones(1,trackMax);
 
 	// Used for plotting the vehicle paths
 	trackt.push_back(SPH.get_initial_time());
 	plotdt = 0.1;	// replot every 100 milliseconds
 	plott = SPH.get_time();
 	t0 = SPH.get_time();
-	tf = 50;	// time final
+	tf = 30;	// time final, when to stop simulation
 
 	lx.resize(1,2);
 	rdx.resize(1,2);
@@ -26,6 +28,8 @@ void simulation::init_simulation() {
 
 void simulation::start_simulation() {
 	GnuplotPipe gp;
+	int thead = 0; // where in the tracking matrix the head is
+
 	for(double t = t0; t < tf; t += SPH.get_dt()) {
 		// Loiter circle locations
 		// Position [x y]
@@ -37,7 +41,7 @@ void simulation::start_simulation() {
 		obx << 	 7, 0,
 				12, 4,
 				16, 2,
-				9,-2,
+				 9,-2,
 				22,-6;
 		
 		// Loiter circle radii
@@ -68,11 +72,12 @@ void simulation::start_simulation() {
 		SPH.sph_sim_step(rdx,lx,lR);
 
 		// Keep track of vehicle paths (or at least the last 100 points)
-		x = append_right(x, SPH.get_x());
-		y = append_right(y, SPH.get_y());
-		u = append_right(u, SPH.get_u());
-		v = append_right(v, SPH.get_v());
+		x.col(thead) = SPH.get_x();
+		y.col(thead) = SPH.get_y();
+		u.col(thead) = SPH.get_u();
+		v.col(thead) = SPH.get_v();
 		trackt.push_back(SPH.get_time());
+		thead = (thead + 1) % trackMax;
 
 		if(x.array().isNaN().any()) {
 			cout << "Something went wrong, NaN detected in x-positions.";
@@ -81,7 +86,7 @@ void simulation::start_simulation() {
 
 		// Plot
 		if(SPH.get_time() >= plott - SPH.get_dt()/10) {
-			plot_veh(SPH,x,y,trackt,lx, obx);
+			plot_veh(SPH,x,y,trackt,lx, obx, thead);
 			plott = plott + plotdt;
 		}
 
@@ -99,37 +104,60 @@ string gnuvec(const MatrixXd& mat, const string& varname) {
 	return o.str();
 }
 
-void simulation::plot_veh(const sph_sim& SPH, const MatrixXd& x, const MatrixXd& y, const vector<double>& trackt, const MatrixXd& lx, const MatrixXd& obx) {
+void simulation::plot_veh(const sph_sim& SPH, const MatrixXd& x, const MatrixXd& y, const vector<double>& trackt, const MatrixXd& lx, const MatrixXd& obx, const int& thead) {
 	ostringstream o;
 	//o << "plot " //<< "(0+word(X,int(t))),(0+word(Y,int(t)))" << ", "			// line ver
 	//			 << "(0+word(X,int(t))),(0+word(Y,int(t))) with points";	// point ver
 	//gp.sendLine("HI",true);
 	
-	gp.sendLine(gnuvec(x, "X"), true);
+	// Set variables "X" and "Y" to be the xy positions of the vehicles
+	/*gp.sendLine(gnuvec(x, "X"), true);
 	gp.sendLine(gnuvec(y, "Y"), true);
+	// Plot mode set to 2D
 	gp.sendLine("set parametric", true);
+	// Configure variable t to iterate through the xy positions
 	gp.sendLine("set trange [1:words(X)]; set samples words(X)", true);
-	gp.sendLine("unset key", true);			// disable legend
-	gp.sendLine("set xrange [-10:40]; set yrange [-10:10]", true);	// set x/y-axis ranges
+	// Disable legend in the corner
+	gp.sendLine("unset key", true);
+	// Set x/y-axis ranges
+	gp.sendLine("set xrange [-10:40]; set yrange [-10:10]", true);
+
+
 	//gp.sendLine("plot (0+word(X,int(t))),(0+word(Y,int(t)))", true);
-	gp.sendLine("plot (0+word(X,int(t))),(0+word(Y,int(t))) with points", true);
-	gp.sendEndOfData();
+	gp.sendLine("plot (0+word(X,int(t))),(0+word(Y,int(t))) with points linetype 6", true);
+	gp.sendEndOfData();*/
+	plot_trails(x,y,thead);
 }
 
+string simulation::gnutrail(const RowVectorXd& pos, const string& varname, const int& thead) {
+	ostringstream o;
+	o << varname << "=\"";
+	for(int i = 0; i < pos.size(); ++i) {
+		int tidx = (thead+i) % trackMax;
+		o << pos(tidx) << " ";
+	}
+	o << "\"";
+	return o.str();
+}
 
+void simulation::plot_trails(const MatrixXd& x, const MatrixXd& y, const int& thead) {
+	for(int i = 0; i < x.rows(); ++i) {
+		gp.sendLine(gnutrail(x.row(i),"X"+to_string(i),thead), true);
+		gp.sendLine(gnutrail(y.row(i),"Y"+to_string(i),thead), true);
+	}
 
-bool wait_for_key () {
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__)  // every keypress registered, also arrow keys
-    cout << endl << "Press any key to continue..." << endl;
-
-    FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
-    _getch();
-#elif defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
-    cout << endl << "Press ENTER to continue..." << endl;
-
-    std::cin.clear();
-    std::cin.ignore(std::cin.rdbuf()->in_avail());
-    std::cin.get();
-#endif
-    return false;
+	gp.sendLine("set parametric", true);
+	gp.sendLine("set trange [1:words(X0)]; set samples words(X0)", true);
+	gp.sendLine("unset key", true);
+	gp.sendLine("set xrange [-10:40]; set yrange [-10:10]", true);
+	ostringstream o;
+	o << "plot ";
+	for(int i = 0; i < x.rows(); ++i) {
+		o << "(0+word(X" << to_string(i) << ",int(t))),(0+word(Y" << to_string(i) << ",int(t)))";
+		if(i+1 < x.rows()) {
+			o << ", ";
+		}
+	}
+	gp.sendLine(o.str(), true);
+	gp.sendEndOfData();
 }
